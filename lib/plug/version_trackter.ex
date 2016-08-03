e23771f76f2075fb8d27c1c0348c7df441f1a50a
Siraset Jirapatchadej <vernomcrp@gmail.com>
2016-08-03 12:43:07 +0700

    add prototype for version tracking plug

---
+++ lib/plug/version_trackter.ex
@@ -0,0 +1,13 @@
+defmodule SimpleWebapp.Plug.VersionTracker do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  @@doc """
+  Detect version then bind to request (conn)
+  """
+  def call(conn, opts) do
+    version = Keyword.fetch!(ops, :version)
+    assign(conn, :version, version)
+  end
+end
